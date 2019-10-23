edcd83005b72f2296725e976db64da585c476e29
fulstaph <fulstaph@gmail.com>
2019-10-23 23:20:05 +0300

    message

---
+++ test.swift
@@ -0,0 +1,21 @@
+import Foundation; 
+
+print("Hello, world!");
+
+func lol() -> () {
+    print("lol func");
+} 
+
+lol();
+
+func multByTwo(_ num: Double) -> Double { return 2.0 * num; }
+
+let yes = true, no = false;
+
+if yes == no { print("what") }
+else { print("makes sense") }
+
+typealias null = nil
+
+
+print(multByTwo(3.14));
